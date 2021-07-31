af63966537c9d00399cb3362be1de5af9a88b40b
Md Maruf Mubin <mobinh264@gmail.com>
2021-08-01 05:49:23 +0600

    Full Fill todo app done

--- js/app.js
+++ js/app.js
@@ -12,43 +12,49 @@ function addTastToList(event){
     event.preventDefault();
     // captureUserInput
     const todo = todoInput.value;
+
+    if(todo == ""){
+        alert("Please add a tesk Fist");
+    }else{
+        
+        //create a div
+        const todoDiv = document.createElement("div");
+
+        // Assign to do div
+        todoDiv.classList.add("todo");
+
+        // create the list element
+        const todoItem = document.createElement("li");
+
+        // add todo-item class to the list
+        todoItem.classList.add("todo-item");
+
+        // add to do to the list
+        todoItem.innerText = todo;
+
+        // atach list ot the div
+        todoDiv.appendChild(todoItem);
+
+        // add div to the ul list 
+        todoList.appendChild(todoDiv);
+
+        // add check button to the list
+        const checkBtn = document.createElement("button");
+        checkBtn.classList.add("check");
+        checkBtn.innerHTML = `<i class="fas fa-check"></i>`;
+        todoDiv.appendChild(checkBtn);
+
+        // add trash button to the list
+        const trashBtn = document.createElement("button");
+        trashBtn.classList.add("trash");
+        trashBtn.innerHTML = `<i class="fas fa-trash"></i>`;
+        todoDiv.appendChild(trashBtn);
+        
+        // remove tast from input fild
+        todoInput.value='';
+
+    }
     //console.log(todo);
-
-    //create a div
-    const todoDiv = document.createElement("div");
-
-    // Assign to do div
-    todoDiv.classList.add("todo");
-
-    // create the list element
-    const todoItem = document.createElement("li");
-
-    // add todo-item class to the list
-    todoItem.classList.add("todo-item");
-
-    // add to do to the list
-    todoItem.innerText = todo;
-
-    // atach list ot the div
-    todoDiv.appendChild(todoItem);
-
-    // add div to the ul list 
-    todoList.appendChild(todoDiv);
-
-    // add check button to the list
-    const checkBtn = document.createElement("button");
-    checkBtn.classList.add("check");
-    checkBtn.innerHTML = `<i class="fas fa-check"></i>`;
-    todoDiv.appendChild(checkBtn);
-
-    // add trash button to the list
-    const trashBtn = document.createElement("button");
-    trashBtn.classList.add("trash");
-    trashBtn.innerHTML = `<i class="fas fa-trash"></i>`;
-    todoDiv.appendChild(trashBtn);
-    
-    // remove tast from input fild
-    todoInput.value='';
 
 }
 
@@ -57,7 +63,10 @@ function checkOrDelete(event){
     //User want ot delete todo
     if( item.classList[0] == "trash" ){
         const todo = item.parentElement; 
-        todo.remove();
+        todo.classList.add("fallen");
+        todo.addEventListener("transitionend",function(){
+            todo.remove();
+        })
     }
     else if( item.classList[0] == "check" ){
         const todo = item.parentElement;
@@ -65,7 +74,4 @@ function checkOrDelete(event){
         item.remove();
     }
 }
-// submitBtn.addEventListener("click",function(event){
-//     event.preventDefault();
-//     console.log("amake click kreche");
-// }) 
+
